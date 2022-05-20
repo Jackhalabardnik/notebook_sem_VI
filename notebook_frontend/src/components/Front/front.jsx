6233b44ca573627c84d67e48b9c40e4b5ae8da9b
Jackhalabardnik <wojcikj2000@protonmail.com>
2022-05-20 18:42:48 +0200

    feat - login front finished

--- notebook_frontend/src/components/Front/front.jsx
+++ notebook_frontend/src/components/Front/front.jsx
@@ -1,25 +1,49 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Login from "../Login/login";
+import {Button} from "react-bootstrap";
 
 const Front = () => {
     return (
         <div>
-            <nav className="bg-primary d-flex justify-content-end">
-                <h2 className="my-auto ms-5 text-white">
+            <nav className="bg-primary d-flex justify-content-end flex-column flex-md-row">
+                <h2 className="ms-md-5 my-3 text-white text-center">
                     Notebook
                 </h2>
-                <div className="w-75">
+                <div className="col-lg-5 col-1">
 
                 </div>
                 <div>
                     <Login/>
                 </div>
             </nav>
-            <div>
-                Yo bro
-                <div>
-                    <Link to="/signup">Signup</Link>
+            <div className="d-flex flex-lg-row flex-md-column">
+                <div
+                    className="d-flex flex-column align-items-center fs-4 align-middle p-4 col-lg-4 col-12 text-center">
+
+                    <ul className="text-start">
+                        <li>
+                            Do you need a place to store your shopping list?
+                        </li>
+                        <li>
+                            Need a place to write down your thoughts?
+                        </li>
+                        <li>
+                            Do you use a self-chat in discord or messenger to keep them in order?
+                        </li>
+                    </ul>
+                    If this is true, you have come to the right place.
+                    <div className="my-4">
+                        <Link to="/signup">
+                            <Button variant="primary" className="fs-3">
+                                Register today
+                            </Button>
+                        </Link>
+                    </div>
+
+                    And create your own notebook for free!
+
                 </div>
+                <img src="/splash.png" alt="splash" className="col-lg-8 col-12 img-fluid"/>
             </div>
         </div>
     )
--- notebook_frontend/src/components/Login/login.jsx
+++ notebook_frontend/src/components/Login/login.jsx
@@ -1,60 +1,67 @@
 import {useState} from "react"
 import axios from "axios"
-import {Button, FloatingLabel, Form} from "react-bootstrap"
+import {Alert, Button, FloatingLabel, Form} from "react-bootstrap"
+import { useFormik } from 'formik';
+import * as yup from "yup";
+
+const loginValidationSchema = yup.object().shape({
+    email: yup.string().email().required().label('Email'),
+    password: yup.string().required().label('Password')
+});
 
 const Login = () => {
-    const [data, setData] = useState({email: "", password: ""})
-    const [error, setError] = useState("")
-    const handleChange = ({currentTarget: input}) => {
-        setData({...data, [input.name]: input.value})
-    };
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const url = "http://localhost:8080/api/auth/login"
-            const {data: res} = await axios.post(url, data)
-            localStorage.setItem("token", res)
-            window.location = "/"
-        } catch (error) {
-            if (
-                error.response &&
-                error.response.status >= 400 &&
-                error.response.status <= 500
-            ) {
-                setError(error.response.data.message)
-            }
-        }
-    }
+    const [error, setError] = useState('')
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: loginValidationSchema,
+        onSubmit: values => {
+            axios.post("http://localhost:8080/api/auth/login", values)
+                .then(response => {
+                    localStorage.setItem("token", response.data);
+                    window.location = "/";
+                })
+                .catch(error => {
+                    if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+                        setError(error.response.data)
+                    }
+                })
+        },
+    });
 
     return (
         <div>
-            <Form onSubmit={handleSubmit} noValidate className="p-3">
-                <div className="d-flex justify-content-end">
-                    <FloatingLabel controlId="inputUserName" label="Username" className="me-3">
+            <Form onSubmit={formik.handleSubmit} noValidate className="p-3">
+                <div className="d-flex flex-column flex-md-row  justify-content-end">
+                    <FloatingLabel controlId="inputUserName" label="Email" className="me-md-3">
                         <Form.Control
-                            type="text"
+                            type="email"
                             name="email"
                             placeholder="Email"
-                            onChange={handleChange}
-                            value={data.email}
-                            isInvalid={error}
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            isInvalid={formik.touched.email && !!formik.errors.email}
                         />
-                        <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid" className="fw-bold bg-dark text-center bg-opacity-75 rounded-3 text-white" >{formik.errors.email}</Form.Control.Feedback>
                     </FloatingLabel>
-                    <FloatingLabel controlId="inputPassword" label="Password" className="me-3">
+                    <FloatingLabel controlId="inputPassword" label="Password" className="me-md-3 my-md-0 my-3">
                         <Form.Control
                             type="password"
                             name="password"
                             placeholder="Password"
-                            onChange={handleChange}
-                            value={data.password}
-                            isInvalid={error}
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            isInvalid={formik.touched.password && !!formik.errors.password}
                         />
-                        <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid" className="fw-bold bg-dark text-center bg-opacity-75 rounded-3 text-white" >{formik.errors.password}</Form.Control.Feedback>
                     </FloatingLabel>
-                    <Button type="submit" variant="primary" className="col-2 border-1 border-white">Sign In</Button>
+                    <Button type="submit" variant="primary" className="col-lg-3 col-md-4 col-12 col-xl-3 border-1 border-white">Sign In</Button>
                 </div>
             </Form>
+            {error && <Alert variant="danger" className="text-center m-2">Error: {error}</Alert> }
         </div>
     )
 }
