02e4e6061822789b3ba4efd8036bd0b0e30fac55
Jackhalabardnik <wojcikj2000@protonmail.com>
2022-05-30 14:40:23 +0200

    style - styled navbar

--- notebook_frontend/src/components/RNavBar/rnavbar.jsx
+++ notebook_frontend/src/components/RNavBar/rnavbar.jsx
@@ -10,7 +10,7 @@ const RNavBar = (props) => {
 
     useEffect(() => {
         const token = localStorage.getItem("token")
-        if(token) {
+        if (token) {
             axios.get(`http://localhost:8080/api/users/logged_in`, {headers: {"authorization": `${token}`}})
                 .then((response) => {
                     setUserData(response.data)
@@ -28,31 +28,37 @@ const RNavBar = (props) => {
     const token = localStorage.getItem("token")
 
     return (
-        <nav className="bg-primary d-flex justify-content-between flex-column flex-md-row align-items-center">
-            <Link to={props.mainlink ? props.mainlink : "/" } className="ms-md-2 my-3 text-white text-center text-decoration-none fs-3 fw-bold">
-                Notebook
-            </Link>
+        <div className="bg-dark">
+            <nav
+                className="bg-light bg-opacity-10 d-flex justify-content-between flex-column flex-md-row align-items-center">
+                <Link to={props.mainlink ? props.mainlink : "/"}
+                      className="ms-md-2 my-3 text-white text-center text-decoration-none fs-3 fw-bold">
+                    Notebook
+                </Link>
 
-            {token ? <div className="d-flex flex-column flex-md-row justify-content-md-center mx-3 align-items-center">
-                    <div className={"text-white fs-5 fw-bold" + (window.location.pathname === "/user" ? "me-0 me-md-4" : "")}>
+                {token ?
+                    <div className="d-flex flex-column flex-md-row justify-content-md-center mx-3 align-items-center">
+                        <div
+                            className={"text-white fs-5 fw-bold" + (window.location.pathname === "/user" ? "me-0 me-md-4" : "")}>
                             {userData.username}
+                        </div>
+                        {
+                            window.location.pathname !== "/user" &&
+                            <Link to="/user" className="m-3">
+                                <Button variant="outline-light">
+                                    Settings
+                                </Button>
+                            </Link>
+                        }
+                        <Button onClick={handleLogout} variant="outline-light mb-3 mb-md-0">
+                            Log out
+                        </Button>
                     </div>
-                    {
-                        window.location.pathname !== "/user" &&
-                        <Link to="/user" className="m-3">
-                            <Button variant="outline-light">
-                                Settings
-                            </Button>
-                        </Link>
-                    }
-                    <Button onClick={handleLogout} variant="outline-light mb-3 mb-md-0">
-                        Log out
-                    </Button>
-                </div>
-                :
-                <Login/>
-            }
-        </nav>
+                    :
+                    <Login/>
+                }
+            </nav>
+        </div>
     )
 }
 export default RNavBar
